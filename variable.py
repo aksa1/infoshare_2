275c3d3e74cf1cb05fe5c39a481404aaeff5636c
aksa1 <35894814+aksa1@users.noreply.github.com>
2018-01-31 20:12:47 +0100

    rozne zmienne wyswiwtlanie typow zmiennych

---
+++ variable.py
@@ -0,0 +1,22 @@
+#int#
+age = 26
+#float"
+temperature = 7.4
+#string"
+name = 'agnieszka'
+#bool"
+programmer = True
+#nonetype#
+nothing = None
+#print(age)
+#print(temperature)
+# @TODO: wyświetl reszte zmiennych na ekranie
+age = 52
+print(age)
+print(type(age))
+age = 'something'
+print(age)
+print(type(age))
+# @todo: wyświetl typ wszystkich zmiennych
+print (nothing)
+print(type(nothing))
