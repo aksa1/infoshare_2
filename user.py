f356b9ad41b3c92393283073b96a6a5474effc6f
aksa1 <35894814+aksa1@users.noreply.github.com>
2018-01-31 20:26:07 +0100

    wyswietlanie imienia

---
+++ user.py
@@ -0,0 +1,9 @@
+#chcemy zeby userowi wyswietlil sie komunikat zeby podal nam dane
+name = input('Podaj swoje imie: ')
+# @todo wysiwtl na ekranie zmienna name
+print(name)
+# 1. the bad way
+print('Twoje imie to: ', name)
+
+
+
