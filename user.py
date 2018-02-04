11bd0a87b1b5f472294876693ab75bbee3d57489
aksa1 <35894814+aksa1@users.noreply.github.com>
2018-02-04 20:24:27 +0100

    refactor - extract - wyjasnienie funkcji

--- user.py
+++ user.py
@@ -1,11 +1,9 @@
 #chcemy zeby userowi wyswietlil sie komunikat zeby podal nam dane
 name = input('Podaj swoje imie: ')
+age = input('Podaj ile masz lat: ')
 # @todo wysiwtl na ekranie zmienna name
 print(name)
-# 1. the bad way
-message = 'Twoje imie to: '
-print('%s' % message, name)
-
-
-
-
+# 1. the best way (message zawiera podane w formularzu dane, w messagu jest
+# pokazane jak sformatować podane dane) dopiero print drukuje wynik
+message = f'Twoje imie to: {name} {age}'
+print(message)
