b76a3bf596ab0fdf9502eee44bf90f87f9c1c3fb
aksa1 <35894814+aksa1@users.noreply.github.com>
2018-02-05 20:27:23 +0100

    if statements

---
+++ task.py
@@ -0,0 +1,9 @@
+digit = input('Podaj liczbe: ')
+#zeby otrzymac ze stringa integera -> skonwertowac na integera
+digit = int(digit)
+
+#@ todo:
+if is_even:
+    print('liczba jest parzysta')
+#@ todo: w przeciwnym przypadku wyświetl 'liczba jest nieparzysta'
+#else:
