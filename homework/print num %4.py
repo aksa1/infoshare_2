03685f059b1db5452903d0049c80af804ead4e5a
aksa1 <35894814+aksa1@users.noreply.github.com>
2018-02-10 23:08:00 +0100

    wydrukuj liczby niepodzielne przez 4

---
+++ homework/print num %4.py
@@ -0,0 +1,12 @@
+# program, ktory wypisze liczby od 0 do 20 poza liczbami podzielnymi przez 4
+# continue
+
+for value in range(0, 21):
+    if value%4 != 0:
+        print(value)
+#continue zbędne
+
+# drukuje wartosci podzielne przez 4
+# for value in range(0, 21):
+#     if value%4 ==0:
+#         print(value)
