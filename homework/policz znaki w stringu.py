ca30e601e10b7dc3842db38853835db4f7059d3e
aksa1 <35894814+aksa1@users.noreply.github.com>
2018-02-18 23:02:44 +0100

    poprawka

--- homework/policz znaki w stringu.py
+++ homework/policz znaki w stringu.py
@@ -1,4 +1,19 @@
 #program obliczajacy liczbe liter i cyfr w stringu
 text = input('Podaj tekst: ')
-ilosc_liter = str(len(text))
-print(ilosc_liter)
+ilosc_liter = 0
+ilosc_cyfr = 0
+ilosc_odstepow = 0
+ilosc_pozostalych_znakow = 0
+for i in text:
+    if i.isalpha():
+        ilosc_liter += 1
+    elif i.isnumeric():
+        ilosc_cyfr += 1
+    elif i.isspace():
+        ilosc_odstepow += 1
+    else:
+        ilosc_pozostalych_znakow += 1
+print("Liter w ciagu: {}".format(ilosc_liter))
+print("Cyfr w ciagu: {}".format(ilosc_cyfr))
+print("odstepow w ciagu: {}".format(ilosc_odstepow))
+print("pozostalcyh znakow: {}".format(ilosc_pozostalych_znakow))
